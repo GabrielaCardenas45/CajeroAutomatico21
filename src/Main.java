d9edeecb507444796982ef185672be566625f600
Maira <110685055+Mairaolaya@users.noreply.github.com>
2023-10-17 14:54:42 -0500

    CageroAutomatico while h

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ class CageroAutomatico2 {
         while (opcion != 3) {
 
             opcion = Integer.parseInt(JOptionPane.showInputDialog("bienvenido al cajero Andromeda\n"
-                    + "1 abonar dinero a la cuenta\n"
+                    + "1 abonar dinero a la cuennta\n"
                     + "2 retirar dinero de la cuenta\n"
                     + "3 gracias por utilizar nuestros servicios"));
             switch (opcion) {
